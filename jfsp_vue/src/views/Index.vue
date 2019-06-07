532d30baa04ce7436f963c1dbfbd5fb4f1e6fd14
ld7979 <ld_9977@163.com>
2019-06-07 16:16:55 +0800

    项目代码上传

---
+++ jfsp_vue/src/views/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <myheader></myheader>
+  </div>
+</template>
+
+<script>
+import myheader from '../components/myHeader'
+export default {
+  name: "Index",
+  data() {
+    return {};
+  },
+  components:{myheader}
+  
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
