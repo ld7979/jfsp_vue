73c3873c1be1109ac25f6a8102d6700607716c9f
ld7979 <ld_9977@163.com>
2019-06-08 13:21:57 +0800

    头部

--- src/components/myHeader.vue
+++ src/components/myHeader.vue
@@ -21,17 +21,56 @@
                 菜单
               </a>
             </div>
-            <div class="cblok clearfix">
-              <div class="citem clearfix">
-                <span>食材</span>
-                <div class=" clearfix">
-                  <a href="javascript:;" target="_blank">猪肉</a>
-                  <a href="javascript:;" target="_blank">猪肉</a>
-                  <a href="javascript:;" target="_blank">猪肉</a>
-                  <a href="javascript:;" target="_blank">猪肉</a>
-                  <a href="javascript:;" target="_blank">猪肉</a>
-                </div>
+            <div class="citem clearfix">
+              <span>食材</span>
+              <div>
+                <a href="javascript:;" target="_blank">猪肉</a>
+                <a href="javascript:;" target="_blank">羊肉</a>
+                <a href="javascript:;" target="_blank">鸡肉</a>
+                <a href="javascript:;" target="_blank">牛肉</a>
+                <a href="javascript:;" target="_blank">鱼</a>
+                <a href="javascript:;" target="_blank">蔬菜</a>
+                <a href="javascript:;" target="_blank">鲜果</a>
+                <a href="javascript:;">更多
+                  <img src="images/header/more2.png">
+                </a>
               </div>
+            </div>
+            <div class="citem clearfix">
+              <span>菜式</span>
+              <div>
+                <a href="javascript:;" target="_blank">下饭菜</a>
+                <a href="javascript:;" target="_blank">快手菜</a>
+                <a href="javascript:;" target="_blank">凉菜</a>
+                <a href="javascript:;" target="_blank">宴客菜</a>
+                <a href="javascript:;" target="_blank">私房菜</a>
+                <a href="javascript:;" target="_blank">素食主义</a>
+                <a href="javascript:;" target="_blank">下酒菜</a>
+                <a href="javascript:;">更多
+                  <img src="images/header/more2.png">
+                </a>
+              </div>
+            </div>
+            <div class="citem clearfix">
+              <span>烘焙</span>
+              <div>
+                <a href="javascript:;" target="_blank">月饼</a>
+                <a href="javascript:;" target="_blank">披萨</a>
+                <a href="javascript:;" target="_blank">蛋挞</a>
+                <a href="javascript:;" target="_blank">布丁</a>
+                <a href="javascript:;" target="_blank">中式糕点</a>
+                <a href="javascript:;" target="_blank">泡芙</a>
+                <a href="javascript:;" target="_blank">松饼</a>
+                <a href="javascript:;">更多
+                  <img src="images/header/more2.png">
+                </a>
+              </div>
+            </div>
+            <div class="cmore">
+               <a href="javascript:;">
+                 查看更多
+                 <img src="images/header/more2.png" alt="">
+               </a>
             </div>
           </div>
         </li>
@@ -47,7 +86,7 @@
       </div>
       <div class="pubt">
         <a href="javascript:;">发布</a>
-        <div class="pubt-box" style="display:none">
+        <div class="pubt-box" style="display:block">
           <a href="javascript:;">发布菜单</a>
           <a href="javascript:;">创建菜单</a>
         </div>
