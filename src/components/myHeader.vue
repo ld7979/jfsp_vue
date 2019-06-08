e2c7eebdbb90564d3222288184177361cc9570bd
ld7979 <ld_9977@163.com>
2019-06-08 12:15:43 +0800

    图片

--- src/components/myHeader.vue
+++ src/components/myHeader.vue
@@ -6,10 +6,18 @@
       </a>
       <ul class="ul_nav">
         <li class="act"><a href="javascript:;">首页</a></li>
-        <li>
+        <li style="position: relative">
           <a href="javascript:;">菜谱
             <span class="nass"></span>
           </a>
+          <div class="ctip" style="display: block">
+            <div class="ctab clearfix">
+              <a href="javascript:;" target="_blank">
+                <img src="images/header/jx3.png" >
+                精选
+              </a>
+            </div>
+          </div>
         </li>
         <li><a href="javascript:;">笔记</a></li>
         <li><a href="javascript:;">主题站</a></li>
