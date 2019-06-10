8a19232673844e8435e54a01a0efd458a5182e72
ld7979 <ld_9977@163.com>
2019-06-10 23:41:55 +0800

    daohang

--- src/components/myHeader.vue
+++ src/components/myHeader.vue
@@ -1,22 +1,22 @@
 <template>
   <header id="header">
     <nav class="nav clearfix">
-      <a class="logo left" href="javascript:;">
-        <img src="images/header/logo3.png" alt="">
-      </a>
+      <router-link class="logo left" to="/">
+        <img src="images/header/logo3.png">
+      </router-link>
       <ul class="ul_nav">
-        <li class="act"><a href="javascript:;">首页</a></li>
-        <li style="position: relative">
-          <a href="javascript:;">菜谱
+        <li class="act"><router-link to="/" @click.native="select($event)">首页</router-link></li>
+        <li style="position: relative" >
+          <router-link @mouseover.native="cmouseOver" @mouseout.native="cmouseOut" to="/c" @click.native="select($event)" >菜谱
             <span class="nass"></span>
-          </a>
-          <div class="ctip" style="display: block">
+          </router-link>
+          <div v-show="cdisp"  class="ctip">
             <div class="ctab clearfix">
-              <a href="javascript:;" target="_blank">
+              <a to="javascript:;" target="_blank">
                 <img src="images/header/jx3.png" >
                 精选
               </a>
-              <a href="javascript:;" target="_blank">
+              <a to="javascript:;" target="_blank">
                 <img src="images/header/menu3.png" >
                 菜单
               </a>
@@ -24,14 +24,14 @@
             <div class="citem clearfix">
               <span>食材</span>
               <div>
-                <a href="javascript:;" target="_blank">猪肉</a>
-                <a href="javascript:;" target="_blank">羊肉</a>
-                <a href="javascript:;" target="_blank">鸡肉</a>
-                <a href="javascript:;" target="_blank">牛肉</a>
-                <a href="javascript:;" target="_blank">鱼</a>
-                <a href="javascript:;" target="_blank">蔬菜</a>
-                <a href="javascript:;" target="_blank">鲜果</a>
-                <a href="javascript:;">更多
+                <a to="javascript:;" target="_blank">猪肉</a>
+                <a to="javascript:;" target="_blank">羊肉</a>
+                <a to="javascript:;" target="_blank">鸡肉</a>
+                <a to="javascript:;" target="_blank">牛肉</a>
+                <a to="javascript:;" target="_blank">鱼</a>
+                <a to="javascript:;" target="_blank">蔬菜</a>
+                <a to="javascript:;" target="_blank">鲜果</a>
+                <a to="javascript:;">更多
                   <img src="images/header/more2.png">
                 </a>
               </div>
@@ -39,14 +39,14 @@
             <div class="citem clearfix">
               <span>菜式</span>
               <div>
-                <a href="javascript:;" target="_blank">下饭菜</a>
-                <a href="javascript:;" target="_blank">快手菜</a>
-                <a href="javascript:;" target="_blank">凉菜</a>
-                <a href="javascript:;" target="_blank">宴客菜</a>
-                <a href="javascript:;" target="_blank">私房菜</a>
-                <a href="javascript:;" target="_blank">素食主义</a>
-                <a href="javascript:;" target="_blank">下酒菜</a>
-                <a href="javascript:;">更多
+                <a to="javascript:;" target="_blank">下饭菜</a>
+                <a to="javascript:;" target="_blank">快手菜</a>
+                <a to="javascript:;" target="_blank">凉菜</a>
+                <a to="javascript:;" target="_blank">宴客菜</a>
+                <a to="javascript:;" target="_blank">私房菜</a>
+                <a to="javascript:;" target="_blank">素食主义</a>
+                <a to="javascript:;" target="_blank">下酒菜</a>
+                <a to="javascript:;">更多
                   <img src="images/header/more2.png">
                 </a>
               </div>
@@ -54,55 +54,89 @@
             <div class="citem clearfix">
               <span>烘焙</span>
               <div>
-                <a href="javascript:;" target="_blank">月饼</a>
-                <a href="javascript:;" target="_blank">披萨</a>
-                <a href="javascript:;" target="_blank">蛋挞</a>
-                <a href="javascript:;" target="_blank">布丁</a>
-                <a href="javascript:;" target="_blank">中式糕点</a>
-                <a href="javascript:;" target="_blank">泡芙</a>
-                <a href="javascript:;" target="_blank">松饼</a>
-                <a href="javascript:;">更多
+                <a to="javascript:;" target="_blank">月饼</a>
+                <a to="javascript:;" target="_blank">披萨</a>
+                <a to="javascript:;" target="_blank">蛋挞</a>
+                <a to="javascript:;" target="_blank">布丁</a>
+                <a to="javascript:;" target="_blank">中式糕点</a>
+                <a to="javascript:;" target="_blank">泡芙</a>
+                <a to="javascript:;" target="_blank">松饼</a>
+                <a to="javascript:;">更多
                   <img src="images/header/more2.png">
                 </a>
               </div>
             </div>
             <div class="cmore">
-               <a href="javascript:;">
+               <a to="javascript:;">
                  查看更多
                  <img src="images/header/more2.png" alt="">
                </a>
             </div>
           </div>
         </li>
-        <li><a href="javascript:;">笔记</a></li>
-        <li><a href="javascript:;">主题站</a></li>
-        <li><a href="javascript:;">商城</a></li>
-        <li><a href="javascript:;">动漫</a></li>
-        <li><a href="javascript:;">药品</a></li>
+        <li :class="{act:activ==task}" v-for="(task,index) of tasks" :key="index" @click="myactive(task)"><router-link  :to="task.method">{{task.name}}</router-link></li>
+        <!-- <li><a to="javascript:;">主题站</a></li>
+        <li><a to="javascript:;">商城</a></li>
+        <li><a to="javascript:;">动漫</a></li>
+        <li><a to="javascript:;">药品</a></li> -->
       </ul>
       <div class="search"> 
         <input type="text" value="搜索菜谱、菜单、食材、用户">
-        <a class="sear" href="javascript:;"></a>
+        <router-link class="sear" to="javascript:;"></router-link>
       </div>
       <div class="pubt">
-        <a href="javascript:;">发布</a>
-        <div class="pubt-box" style="display:block">
-          <a href="javascript:;">发布菜单</a>
-          <a href="javascript:;">创建菜单</a>
+        <router-link @mouseover.native="fmouseOver" @mouseout.native="fmouseOut" to="javascript:;">发布</router-link>
+        <div v-show="fdisp" class="pubt-box">
+          <router-link to="javascript:;">发布菜单</router-link>
+          <router-link to="javascript:;">创建菜单</router-link>
         </div>
       </div>
       <div class="perinfo">
-        <a href="#/login">登录</a> | 
-        <a href="#/reg">注册</a>
+        <router-link to="/login">登录</router-link> | 
+        <router-link to="/reg">注册</router-link>
       </div>
     </nav>
   </header>
 </template>
 
 <script>
+import { constants } from 'crypto';
 export default {
   data(){
-    return {}
+    return {
+      cdisp:false,
+      fdisp:false,
+      tasks:[
+        {name:"笔记",method:"/"},
+        {name:"主题站",method:"/"},
+        {name:"商城",method:"/"},
+        {name:"动漫",method:"/"},
+        {name:"药品",method:"/"},
+      ],
+      activ:0,
+    }
+  },
+  methods:{
+    cmouseOver(){
+      this.cdisp=true;
+    },
+    cmouseOut(){
+      this.cdisp=false;
+    },
+    fmouseOver(){
+      this.fdisp=true;
+    },
+    fmouseOut(){
+      this.fdisp=false;
+    },
+    myactive(task){
+      this.activ=task;
+    },
+    select($event){
+       if(location.href==$event.target.href){
+         $event.target.parentElement.className="act";
+       }
+    }
   }
 }
 </script>
