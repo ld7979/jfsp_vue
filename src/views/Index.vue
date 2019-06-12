e41e7c06e0a27b6cee1c05c0952f075b4227d825
LiDian <Ld@tedu.cn>
2019-06-12 20:27:28 +0800

    biji

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -4,7 +4,7 @@
     <banner></banner>
     <div id="i_content" class="clearfix">
       <!-- 精选菜谱 -->
-      <p class=i_title>每日精选菜谱 <router-link to="">更多&gt;</router-link></p>
+      <p class="i_title">每日精选菜谱 <router-link to="">更多&gt;</router-link></p>
       <ul class="recipe-list clearfix">
         <li class="i_item">
           <router-link class="cover br8" to="" target="_blank"><img src="images/index/01.jpeg"></router-link>
@@ -42,6 +42,93 @@
             <router-link class="name text-lips" to="" target="_blank">#甜粽VS咸粽，你是哪一党？#西米水晶粽</router-link>
             <p class="author text-lips">by 
               <router-link class="author_name text-lips" to="" target="_blank">琥珀maggie</router-link>
+              <router-link class="progr" style="background-position: 0 -22px;" to="" target="_blank"></router-link>
+            </p>
+          </div>
+        </li>
+        <li class="i_item">
+          <router-link class="cover br8" to="" target="_blank"><img src="images/index/05.JPG"></router-link>
+          <div>
+            <router-link class="name text-lips" to="" target="_blank">鸡米花</router-link>
+            <p class="author text-lips">by 
+              <router-link class="author_name text-lips" to="" target="_blank">雅雅杨小厨</router-link>
+              <router-link class="progr" style="background-position: 0 -22px;" to="" target="_blank"></router-link>
+            </p>
+          </div>
+        </li>
+        <li class="i_item">
+          <router-link class="cover br8" to="" target="_blank"><img src="images/index/06.jpeg"></router-link>
+          <div>
+            <router-link class="name text-lips" to="" target="_blank">#甜粽VS咸粽，你是哪一党？#海蛎子西葫芦饺子</router-link>
+            <p class="author text-lips">by 
+              <router-link class="author_name text-lips" to="" target="_blank">踏月色而来7737</router-link>
+              <router-link class="progr" style="background-position: 0 -22px;" to="" target="_blank"></router-link>
+            </p>
+          </div>
+        </li>
+        <li class="i_item">
+          <router-link class="cover br8" to="" target="_blank"><img src="images/index/07.jpg"></router-link>
+          <div>
+            <router-link class="name text-lips" to="" target="_blank">蒜香孜然鸡翅</router-link>
+            <p class="author text-lips">by 
+              <router-link class="author_name text-lips" to="" target="_blank">果果郭美食记</router-link>
+              <router-link class="progr" style="background-position: 0 -22px;" to="" target="_blank"></router-link>
+            </p>
+          </div>
+        </li>
+        <li class="i_item" style="margin-right: 0">
+          <router-link class="cover br8" to="" target="_blank"><img src="images/index/08.jpeg"></router-link>
+          <div>
+            <router-link class="name text-lips" to="" target="_blank">酸酸甜甜滴茄汁大虾</router-link>
+            <p class="author text-lips">by 
+              <router-link class="author_name text-lips" to="" target="_blank">霖的吃货铺</router-link>
+              <router-link class="progr" style="background-position: 0 -22px;" to="" target="_blank"></router-link>
+            </p>
+          </div>
+        </li>
+      </ul>
+      <p class="i_title" style="margin-top:40px;">笔记<router-link to="">更多&gt;</router-link></p>
+      <ul class="recipe-list clearfix">
+        <li class="i_item">
+          <router-link class="cover br8" to="" target="_blank"><img src="images/index/09.jpg"></router-link>
+          <div>
+            <router-link class="name text-lips" to="" target="_blank">每一个人的心中 都有一种历久弥香的味道 是食物 也是想念 可以满足味蕾 也能慰籍灵魂 生活中的美好不只是来自食物 但少了美味的食物 生活会有点寡淡索然 闺女现在念高三了 已经进入紧张的复习阶段  学习很累 只有早饭在家里吃 每天清晨4.50起床 精心给闺女准备早餐 每一餐都不重样  搭配美美的碗碟 闺女吃的舒心 这一天心情都是美美的而 时光啊 也这么一天天 一点点走远了 我只希望 宝贝闺女在长大成</router-link>
+            <p class="author text-lips">
+              <img class="headicon" src="images/index/10.jpeg"> 
+              <router-link class="author_name text-lips" to="" target="_blank">策马苍穹</router-link>
+              <router-link class="progr" style="background-position: 0 -22px;" to="" target="_blank"></router-link>
+            </p>
+          </div>
+        </li>
+        <li class="i_item">
+          <router-link class="cover br8" to="" target="_blank"><img src="images/index/11.jpeg"></router-link>
+          <div>
+            <router-link class="name text-lips" to="" target="_blank">我最爱的餐具🌵都是我爱的 保证让你们满意✌️</router-link>
+            <p class="author text-lips">
+              <img class="headicon" src="images/index/12.jpeg"> 
+              <router-link class="author_name text-lips" to="" target="_blank">踏月色而来7737</router-link>
+              <router-link class="progr" style="background-position: 0 -22px;" to="" target="_blank"></router-link>
+            </p>
+          </div>
+        </li>
+        <li class="i_item">
+          <router-link class="cover br8" to="" target="_blank"><img src="images/index/13.jpg"></router-link>
+          <div>
+            <router-link class="name text-lips" to="" target="_blank">这是啥花？🌸~娃赊回来的花😂</router-link>
+            <p class="author text-lips">
+              <img class="headicon" src="images/index/14.jpg"> 
+              <router-link class="author_name text-lips" to="" target="_blank">QQ茜</router-link>
+              <router-link class="progr" style="background-position: 0 -22px;" to="" target="_blank"></router-link>
+            </p>
+          </div>
+        </li>
+        <li class="i_item" style="margin-right: 0">
+          <router-link class="cover br8" to="" target="_blank"><img src="images/index/15.jpeg"></router-link>
+          <div>
+            <router-link class="name text-lips" to="" target="_blank">太太乐爱得味道让她尝到</router-link>
+            <p class="author text-lips">
+              <img class="headicon" src="images/index/16.jpeg"> 
+              <router-link class="author_name text-lips" to="" target="_blank">四毛家的小窝</router-link>
               <router-link class="progr" style="background-position: 0 -22px;" to="" target="_blank"></router-link>
             </p>
           </div>
